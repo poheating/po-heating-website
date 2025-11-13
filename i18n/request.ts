cd2805d9147d4c9153d1b0af9eb190049154f9f8
player0307@wp.pl <player0307@wp.pl>
2025-11-13 22:29:48 +0100

    push changes

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,10 +1,16 @@
-import { DEFAULT_LOCALE } from "../i18n";
 import { getRequestConfig } from "next-intl/server";
 import { headers } from "next/headers";
+import { LOCALES, DEFAULT_LOCALE } from "../i18n";
 
 export default getRequestConfig(async () => {
-  const locale = (await headers()).get("x-next-intl-locale") || DEFAULT_LOCALE;
-  
+  const headersList = await headers();
+  const locale = headersList.get("x-next-intl-locale") || DEFAULT_LOCALE;
+
+  // Walidacja locale
+  if (!LOCALES.includes(locale as any)) {
+    throw new Error(`Invalid locale: ${locale}`);
+  }
+
   return {
     locale,
     messages: (await import(`../locales/${locale}.json`)).default,
